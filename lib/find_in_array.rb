ef036d1fd6260edf0b1cdcf03ce31de05ba3a955
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-09 15:14:45 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
   count = 0
-  while count < array.lenght do
+  while count < array.length do
     puts array[count]===value_to_find
     count += 1
   end
